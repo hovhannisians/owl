d46f73c14367c3877445ac1f3cad96747085ddcd
hovhannisians <51156797+hovhannisians@users.noreply.github.com>
2019-11-09 23:00:38 +0400

    Some New Changes

--- templates/content.php
+++ templates/content.php
@@ -1,14 +1,18 @@
 <div class="menu">
         <nav>
             <ul>
-                <li><a href="#"><img src="d" alt=""> Hello, <?php echo "Alen" ?>!</a></li>
-                <li><a href="#">Messages</a></li>
+                <li><a href="#"><img class="profile-pic" src="d" alt="x"> Hello, <?php echo "Alen" ?>!</a></li>
+                <li><a href="#"><img class="icon" src="assets/icons8-unread-messages-100.png ik" alt="">Messages</a></li>
                 <li><a href="#">Settings</a></li>
             </ul>
         </nav>
 </div>
 <div class="c-post">
     <div class="container">
-        <textarea name="post" id="post-create" cols="30" rows="10"></textarea>
+        <div class="user-info-post">
+            <img class="profile-pic" src="d" alt="h"><h5><?php echo "Alen Hovhannisians"; ?></h5>
+        </div>
+        <textarea name="post" id="post-create" cols="30" rows="10"></textarea> <br>
+        <button style="float: right;" type="submit">Post now</button>
     </div>
 </div>
